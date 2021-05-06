6dc9b97383342ba738ec9dac648a56b417e3b725
DaniPinelli <danielpinelli.fullstack@gmail.com>
2021-05-06 12:31:59 -0300

    Finish changes

--- index.js
+++ index.js
@@ -1,43 +1,70 @@
-//'use strict'
+'use strict'
 
 
-let counter = 0;
-let answer1 = prompt("Welcome to 5 Question's Game! Please enter your name here", "");
-if (answer1 === null || answer1 === "") {
-    alert("Ok, let`s play: Anonimous");
+
+let player = prompt("Welcome to 5 Question's Game! Please enter your name here: ", "");
+if (player === null || player === "") {
+    alert("Ok, let`s play Anonimous!");
 } else {
-    alert("Ok, " + answer1 + " this game will be a piece of cake!");
+    alert("Ok, " + player + " this game will be a piece of cake!");
 }
 
 
-let answer2 = prompt("You`re playing by 5 points... Which is the best city in the world?", "");
-if (answer2 === null || answer2 === "") {
+let answer1 = prompt("You`re playing by 5 points... Which is the best city in the world?", "");
+let counter = 0;
+let answer1Lower = answer1.toLowerCase();
+if (answer1Lower === null || answer1Lower === "") {
     alert("I did't receive any response.");
-} else if (answer2 != "Rosario") {
+} else if (answer1Lower != "rosario") {
     alert("Sorry... You where close :(");
 } else {
+
     alert("Well done! You have 5 points");
     counter = counter + 5;
 }
 
-let answer3 = prompt("What is the name of the longest river in the world?", "");
-if (answer3 === null || answer3 === "") {
+let answer2 = prompt("What is the name of the longest river in the world?", "");
+let answer2Lower = answer2.toLowerCase();
+if (answer2Lower === null || answer2Lower === "") {
     alert("I did't receive any response.");
-} else if (answer3 != "Nilo")
-    alert("Sorry... Wrong answer but still have 5 points");
+} else if (answer2Lower != "nilo")
+    alert("Sorry... Wrong answer but still have " + counter + " points");
 else {
     counter = counter + 10;
     alert("Very Well! You're stronger than He-man! Take your 10 points master of the universe!");
-    alert("You have 15 points!")
+    alert("You have " + counter + " points!")
 
 }
 
-let answer4 = prompt("What is the country that is shaped like a boot?", "");
-if (answer4 === null || answer4 === "") {
+let answer3 = prompt("What is the country that is shaped like a boot?", "");
+let answer3Lower = answer3.toLowerCase();
+if (answer3Lower === null || answer3Lower === "") {
     alert("I did't receive any response.");
-} else if (answer4 != "Italy") {
-    alert("Sorry... Wrong answer but still have " + contador + " points");
+} else if (answer3Lower != "italy") {
+    alert("Sorry... Wrong answer but still have " + counter + " points");
 } else {
     counter = counter + 10;
-    alert("Great! You have " + contador + "  points!");
+    alert("Great! You have " + counter + "  points!");
+}
+
+let answer4 = prompt("What is the city of skyscrapers?", "");
+let answer4Lower = answer4.toLowerCase();
+if (answer4Lower === null || answer4Lower === "") {
+    alert("I did't receive any response.");
+} else if (answer4Lower != "new york") {
+    alert("Wrong answer :( You have " + counter + " points");
+} else {
+    counter = counter + 10;
+    alert("Excellent! You have " + counter + "  points!");
+}
+
+let answer5 = prompt("What is the most abundant chemical element in the earth's crust?", "");
+let answer5Lower = answer5.toLowerCase();
+if (answer5Lower === null || answer5Lower === "") {
+    alert("I did't receive any response. You finish the game with " + counter + " points");
+} else if (answer5Lower != "oxygen") {
+    alert("Wrong! You finish the game with " + counter + " points");
+} else {
+    counter = counter + 10;
+    alert("Very smart! You finish the game with " + counter + "  points!");
 }
